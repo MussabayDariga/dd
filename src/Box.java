716ed2b72ecadb91433744245a313722fffae7a7
MussabayDariga <dariga.musabaeva@gmail.com>
2017-11-09 20:22:12 +0300

    My first project

---
+++ src/Box.java
@@ -0,0 +1,30 @@
+class Box {
+    double w;
+    double h;
+    double d;
+    double volume(){
+        return w * h * d;
+    }
+    void setDim(double w, double h, double d){
+        this.w = w;
+        this.h = h;
+        this.d = d;
+    }
+}
+class BoxDemo5{
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+
+        mybox1.setDim(10, 20, 15);
+        mybox2.setDim(3, 6, 9);
+        // setDim для установки значений
+
+        vol = mybox1.volume();
+        System.out.println("O ="+vol);
+
+        vol = mybox2.volume();
+        System.out.println("O ="+vol);
+    }
+}
---
+++ src/Box158.java
@@ -0,0 +1,55 @@
+class Box158 {
+    double width;
+    double height;
+    double depth;
+
+    Box158(){
+        System.out.println("Konstruktor object");
+        width = 3;
+        height = 3;
+        depth = 3;
+    }
+    double volume() {
+        return width * height * depth;
+    }
+ /*   void setDim(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+    }
+*/
+}
+
+
+class BoxDemo6 {
+    public static void main(String[] args) {
+        Box158 myb1 = new Box158();
+        Box158 myb2 = new Box158();
+
+        double vol;
+
+        vol = myb1.volume();
+        System.out.println("Объем=" + vol);
+
+        vol = myb2.volume();
+        System.out.println("Объем=" + vol);
+
+    }
+}
+/*class BoxDemo7{
+        public static void main(String[] args) {
+            Box158 myb1 = new Box158();
+            Box158 myb2 = new Box158();
+            double vol;
+
+            myb1.setDim( 10, 20, 15);
+            myb2.setDim(3, 6, 9);
+
+            vol = myb1.volume();
+            System.out.println("Объем="+vol);
+
+            vol = myb2.volume();
+            System.out.println("Объем="+vol);
+        }
+}
+*/
